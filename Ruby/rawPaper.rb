2c1a7fb6ddf8afcb767094e7a64e30a5324e8319
Ricky Barillas <ricky.barillas@gmail.com>
2015-12-28 01:41:44 -0500

    todo list
    Things that need to be done

--- Ruby/rawPaper.rb
+++ Ruby/rawPaper.rb
@@ -1,3 +1,11 @@
+=begin
+    The following looks at r/ruby and takes the 30 top links of all time and
+    prints a list of the ones that are definetely pictures.
+    I need to find a way to download and put them in a directory.
+    After that I'll need to find a way to run applescripts off
+    ruby.
+    Also a nice interface
+=end
 require 'redd'
 
 #authorization
